47ee521d562746aae69b161ba5ca3b9a4055a2ce
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-06-22 16:05:54 -0500

    added security against users putting in more than 60 seconds and messing up the system. It now takes the remaining seconds and puts them in the seconds field while adding however many minutes can be made with the seconds into the minute field

--- public/js/rps.js
+++ public/js/rps.js
@@ -6,16 +6,34 @@
 		timer();
 	});	
 	function timer() {
-		var minute = ($("#minute").val()) * 60;
+		var minute = $("#minute").val();
+		var minuteseconds = minute * 60;
 		var second = $("#second").val();
-		var total = minute + second;
+		var total = minuteseconds + second;
+
+	// If the user puts in more than 60 seconds, converts it into the appropriate amount of minutes and puts the remaining value in seconds
+		if(second >= 60) {
+			minute = (parseInt(minute) + parseInt(Math.floor(second / 60)));
+			console.log(second % 60);
+			var leftover = second % 60;
+			if(leftover == 0) {
+				second = "00";
+			} else {
+				second = second % 60;
+			}
+
+		}
+		$("#minutecount").html(minute);
+		$("#secondcount").html(second);
 		var remainder = total % 60;
 		console.log(remainder);
 		var go = setInterval(function() {
 			if(total < 0) {
 				clearInterval(go);
 			} else {
-				
+				if(second == 0) {
+
+				}
 			}
 		});
 	}
